1874c2f95461778f6a2c3cd645811f3697d72b27
АЛЕКСАНДР ЛОЗИЦКИЙ <avtolik2015@outlook.com>
2022-11-20 15:56:31 +0300

    Homework_1

---
+++ homework_1/Program.cs
@@ -0,0 +1,61 @@
+﻿Console.Write("a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b)
+{
+
+    Console.Write("a = " + a + "; b = " + b + " -> max =  " + a);    //  a = 5; b = 7 -> max = 7
+}
+else
+{
+    Console.Write("a = " + a + "; b = " + b + " -> max =  " + b);
+}
+
+/*Console.Write("a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+
+Console.Write(a + ", " + b + ", " + c + " -> " + max);*/      //  2, 3, 7 -> 7
+
+
+/*Console.Write("a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a % 2 == 0) 
+   Console.Write(a + " -> да");           //  4 -> да
+
+else 
+   Console.Write(a + " -> нет");*/
+
+
+/*Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+
+Console.Write(N + " -> ");
+
+while(count <= N) 
+{
+    if (count % 2 == 0)
+        Console.Write(count + ", ");   //  8 -> 2, 4, 6, 8
+        count += 2;
+}*/
+
+
+   
+   
+     
+  
+
+    
+  

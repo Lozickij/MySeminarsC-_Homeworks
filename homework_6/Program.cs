e3c23567c98db2862ec10a0608ef727026e70155
АЛЕКСАНДР ЛОЗИЦКИЙ <avtolik2015@outlook.com>
2022-11-30 15:39:20 +0300

    homework_6

---
+++ homework_6/Program.cs
@@ -0,0 +1,69 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+// Считаем, что ввод М чисел вручную не является обязательным условием. .?
+int[] CreateRandomArray (int size, int minValue, int maxValue)
+{
+    int[] newArray = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        newArray[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return newArray;
+}
+
+void ShowArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+} 
+
+int PositiveNumbersCount (int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0) count++;
+    }
+    return count;
+}
+
+Console.Write("Введите размер массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальное значение элемента: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение элемента: ");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateRandomArray (a, min, max);
+ShowArray (myArray); 
+Console.Write($"-> {PositiveNumbersCount (myArray)}"); 
+
+
+//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+//Предполагаю, что данные координнаты и есть точка пересечения. Но это не точно!
+
+/* void FindPoint(double k1, double b1, double k2, double b2)
+{
+    double x = (b2 - b1) / (k1 - k2); 
+    double y = k1 * (b2 - b1) / (k1 - k2) + b1;
+    Console.Write($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ( {Math.Round(x, 2)}; {Math.Round(y, 2)})");
+}
+
+Console.Write("Введите b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+FindPoint(k1, b1, k2, b2); */
